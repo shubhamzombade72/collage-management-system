802c1650bf78bf0f76ca0b3153e1901160d83684
= <=>
2024-10-04 10:37:27 +0530

    updates

--- CollegeManagementSystem/Depart_view.py
+++ CollegeManagementSystem/Depart_view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.http import response
+# from django.http import response
 from Departments.models import Department
 
 def department_list(request):
